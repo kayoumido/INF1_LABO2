6c983cc9e5d947184cc8fd1a6eaa90e370a1b8c4
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2018-10-09 15:49:36 +0200

    Add decimal convertion to any base

---
+++ main.cpp
@@ -0,0 +1,50 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : Labo2 
+ Fichier     : main.cpp
+ Auteur(s)   : Doran Kayoumi , Soulaymane Lamrani , Wènes Limem
+ Date        : 09.10.2018
+
+ But         : <à compléter>
+
+ Remarque(s) : <à compléter>
+
+ Compilateur : MinGW-g++ 
+ -----------------------------------------------------------------------------------
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+   // Déclaration des variables 
+   long long entierAConvertir,entierAConvertirCopy; 
+   int base,reste; 
+   string strReste,strEntier;
+   
+   cout << "Entrez un nombre entier" << endl;
+   cin >> entierAConvertir;
+   cout << "Entrez une base" << endl; 
+   cin >> base; 
+   entierAConvertirCopy = entierAConvertir;
+   while (entierAConvertirCopy !=0) {
+         reste = entierAConvertirCopy % base ;
+
+      if (reste < 10) { //Pour les bases inférieurs a 10 
+         strReste = '0' + reste;
+      }
+      else {
+         strReste = reste - 10 + 'A';
+      }
+      strEntier = strReste + strEntier;
+
+      entierAConvertirCopy /= base;  
+   }
+   cout << entierAConvertir << " en base " << base << " s'ecrit " << strEntier << endl;
+   return EXIT_SUCCESS;
+}
