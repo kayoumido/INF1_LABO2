3e3986e65515c6c7e2899d010c66a47e06025d9b
Doran Kayoumi <dorankayoumi@gmail.com>
2018-10-11 15:15:51 +0200

    Fix precision problems

--- main.cpp
+++ main.cpp
@@ -5,9 +5,7 @@
  Auteur(s)   : Doran Kayoumi , Soulaymane Lamrani , Wènes Limem
  Date        : 09.10.2018
 
- But         : <à compléter>
-
- Remarque(s) : <à compléter>
+ But         : Convertisseur de entier/décimale vers n'importe quelle base
 
  Compilateur : MinGW-g++ 
  -----------------------------------------------------------------------------------
@@ -21,12 +19,13 @@
 
 using namespace std;
 
-int main() {
+int main() { 
    const string DIGITS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
    
    long double givenRealNumber;
    unsigned basis;
    unsigned precision;
+
    // ask for user input. 
    // number to convert, base to convert to and the wanted precision
    cout << "Entrez un nombre réel" << endl; 
@@ -39,40 +38,62 @@ int main() {
    cin >> precision; 
    
    // get absolute value
-   long double realNumber = fabs(givenRealNumber);
+   long double realNumber = trunc(fabs(givenRealNumber));
+
+
+   int power = 0;
+   
+   // find basis highest possible power within the given number
+   // another solution we found was to use logarithms to find the highest power
+   //    (int)trunc(log(realNumber)/log(basis));   
+   //
+   // the while condition is performed on the next power 
+   //    since we know that the previous one is within the range. 
+   //    Otherwise we'll be one over the max.
+   while (pow(basis, power + 1) < realNumber) {
+      power++;
+   }
    
    // extract integer part of the given number
-   long long integerPart = trunc(realNumber);   
-   double rest;
-   string valueToDisplay;
+   long double quotient;
+   string convertedNumber;
    
-   while (integerPart != 0) {
-     rest = double(integerPart % basis);
-     
-     valueToDisplay = DIGITS[(char)rest] + valueToDisplay;
-     integerPart /= basis;  
+   // convert given number into the chosen basis
+   //    to do this, we start by dividing the the chosen number with 
+   //    with the basis highest power then we go done utile we reach a power of 0.
+   //    e.g.: realNumber     = 4
+   //          basis          = 2
+   //          power          = 2
+   //          4 / 2^2
+   while (power >= 0) {
+     quotient = trunc(realNumber / pow(basis, power));
+     // find the equivalent digit.
+     convertedNumber += DIGITS[(char)quotient];
+     realNumber = realNumber - (quotient * pow(basis, power));
+     power--;
    }
-   valueToDisplay += '.';
+   // check if a precision is wanted
+   if (precision > 0)
+      convertedNumber += '.';
 
-   //Conversion de la partie decimale ; 
-   //Extraction de la partie decimale ; 
-
-   long double decimal = realNumber - trunc(realNumber);
+   // extract the decimal portion of the number
+   long double decimal = givenRealNumber - trunc(givenRealNumber);
    
-   for (unsigned i = 0; i < precision ; i++){
-     decimal *= basis; 
-     //Extraction de la partie Enitere apres chaques mulitplication 
+   for (unsigned i = 0; i < precision ; i++) {
+     decimal *= basis;
+     
+     // extract integer part of decimal
      int numberToConvert = (int)decimal;
      
      decimal -= numberToConvert;
 
-     valueToDisplay += DIGITS[numberToConvert];
+     convertedNumber += DIGITS[numberToConvert];
    }
    
-   valueToDisplay = givenRealNumber < 0 ? "-" + valueToDisplay : valueToDisplay;
-   //affichage 
+   convertedNumber = givenRealNumber < 0 ? "-" + convertedNumber : convertedNumber;
+   // display results
    cout << givenRealNumber << " en base " << basis;
-   cout << " s'ecrit " << valueToDisplay << " avec "<< precision << " chiffres"
+   cout << " s'ecrit " << convertedNumber << " avec "<< precision << " chiffres"
      << " apres la virgule" << endl; 
 
   
