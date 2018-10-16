2dd01164644ec0fcaab237f02300f73a9d333cb1
Doran Kayoumi <dorankayoumi@gmail.com>
2018-10-16 18:14:29 +0200

    Remove type casting

--- main.cpp
+++ main.cpp
@@ -5,12 +5,7 @@
  Auteur(s)   : Doran Kayoumi , Soulaymane Lamrani , Wènes Limem
  Date        : 09.10.2018
 
- But         : Réaliser un programme permettant à un utilisateur de saisir 
-               un nombre en base 10 
-               ainsi qu'une autre base (allant jusqu'à la base 36) 
-               et d'obtenir en retour son nombre converti dans la base fournie.
-
- Remarque(s) : On suppose la saisie utilisateur correcte.
+ But         : Convertisseur d'entier vers n'importe quelle base. [1-36]
 
  Compilateur : MinGW-g++ 
  -----------------------------------------------------------------------------------
@@ -39,13 +34,13 @@ int main() {
    cin >> basis;
    
    // get absolute value
-   long long integerNumber = (int)trunc(abs(givenIntegerNumber));
+   long long integerNumber = trunc(abs(givenIntegerNumber));
    
    int remainder;
    string convertedInteger;
    
    while (integerNumber != 0) {
-      remainder = (int)integerNumber % basis;
+      remainder = integerNumber % basis;
 
       convertedInteger = DIGITS[(char)remainder] + convertedInteger;
       integerNumber /= basis;  
